36176bb4c29e57bc51d66218623e826b42a6af55
Amilosz <35466728+Amilosz@users.noreply.github.com>
2020-02-01 11:17:50 +0100

    Add files via upload

---
+++ DiscoverServer/src/test/java/com/github/DiscoverServer/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.DiscoverServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
